b73628ba14a5fe99913dbbae5ba025424ef8100f
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-23 00:23:37 +0900

    clean up

--- euler/prime.py
+++ euler/prime.py
@@ -2,15 +2,11 @@ WHEEL30 = [7, 11, 13, 17, 19, 23, 29, 31]
 
 
 def maybe_prime():
-    yield 2
-    yield 3
-    yield 5
+    for n in [2, 3, 5]:
+        yield n
 
-    count = 0
     base = 0
     while True:
-        yield WHEEL30[count] + base
-        count += 1
-        if count >= len(WHEEL30):
-            count = 0
-            base += 30
+        for n in WHEEL30:
+            yield n + base
+        base += 30
